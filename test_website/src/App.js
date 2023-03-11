82df78f2a13633a9f6764a6634292bbfbc5bab26
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-11 23:38:42 +0100

    feat : router Homme page

--- test_website/src/App.js
+++ test_website/src/App.js
@@ -1,1 +1,15 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import React from "react";
 
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
