80730dc5428bc51be76e888e27fe42f9b0054bd6
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-14 00:14:11 +0100

    feat : update style

--- test_website/src/components/Nav.js
+++ test_website/src/components/Nav.js
@@ -3,21 +3,21 @@ import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <ul className="bg-gray-900 flex-col items-center items-around h-screen sm:flex-row flex justify-around w-[100%] mt-10 sm:h-auto">
+    <ul className="flex-col items-center items-around h-screen sm:flex-row flex justify-around w-[100%] mt-10 sm:h-auto">
       <NavLink
-        className="flex items-center justify-center border-gray-500 border-solid border-2 h-[100%] w-[100%] hover:bg-gray-400 hover:text-black"
+        className="bg-gray-800 flex items-center justify-center p-10 rounded-xl border-gray-500 border-solid border-2 h-[100%] w-[100%] hover:bg-gray-400 hover:text-black"
         to="/"
       >
         <li>Home</li>
       </NavLink>
       <NavLink
-        className="flex items-center justify-center border-gray-500 border-solid border-2 h-[100%] w-[100%] hover:bg-gray-400 hover:text-black"
+        className="bg-gray-800 flex items-center justify-center p-10 rounded-xl border-gray-500 border-solid border-2 h-[100%] w-[100%] hover:bg-gray-400 hover:text-black"
         to="/"
       >
         <li>Mon CV</li>
       </NavLink>
       <NavLink
-        className="flex items-center justify-center border-gray-500 border-solid border-2 h-[100%] w-[100%] hover:bg-gray-400 hover:text-black"
+        className="bg-gray-800 flex items-center justify-center p-10 rounded-xl border-gray-500 border-solid border-2 h-[100%] w-[100%] hover:bg-gray-400 hover:text-black"
         to="/"
       >
         <li>Profile</li>
--- test_website/src/components/Page.js
+++ test_website/src/components/Page.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Page = (props) => {
   const { children } = props;
-  return <div className="bg-gray-900 text-gray-400">{children}</div>;
+  return (
+    <div className="h-screen overflow-auto bg-gray-900 text-gray-400">
+      {children}
+    </div>
+  );
 };
 
 export default Page;
