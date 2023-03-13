cdfced275de05434d765a3fb9c80c5ad9121c987
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-14 00:07:06 +0100

    feat : style hover link

--- test_website/src/components/Page.js
+++ test_website/src/components/Page.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Page = (props) => {
   const { children } = props;
-  return <div className="bg-gray-900 text-white">{children}</div>;
+  return <div className="bg-gray-900 text-gray-400">{children}</div>;
 };
 
 export default Page;
