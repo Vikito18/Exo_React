8e07a30de1204596c305189abd4fa99a32f980b8
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-15 21:54:30 +0100

    feat : tab

--- countries_api/src/App.js
+++ countries_api/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center bg-gray-300 h-screen">
+      <h1 className="text-3xl font-extrabold p-20">Country Single Page</h1>
+      <ul className="text-white flex flex-row text-center justify-around w-[80%] bg-gray-800 rounded-full p-2">
+        <li className="hover:bg-gray-500 w-[100%] rounded-full">Europe</li>
+        <li className="hover:bg-gray-500 w-[100%] rounded-full ">Afrique</li>
+        <li className="hover:bg-gray-500 w-[100%] rounded-full ">Amérique</li>
+        <li className="hover:bg-gray-500 w-[100%] rounded-full ">Asie</li>
+        <li className="hover:bg-gray-500 w-[100%] rounded-full ">Océanie</li>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
