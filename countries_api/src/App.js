f19bd6bff2982dbfba8a00935ff430312380ac36
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-15 22:25:12 +0100

    feat : update responsive

--- countries_api/src/App.js
+++ countries_api/src/App.js
@@ -4,7 +4,7 @@ import Countries from "./component/Countries";
 const App = () => {
   return (
     <div className="flex flex-col items-center bg-gray-300 h-screen">
-      <h1 className="text-3xl font-extrabold p-10 text-center w-[100%]">
+      <h1 className="text-2xl sm:text-3xl font-extrabold p-10 text-center w-[100%]">
         Country Single Page
       </h1>
       <ul className="text-white text-xs sm:text-base flex flex-row text-center justify-around w-[95%] bg-gray-800 rounded-full p-2">
