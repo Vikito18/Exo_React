0361bd4ad3280e0f728a813c16e42c57b86a409f
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-15 22:40:31 +0100

    feat : useState initialised

--- countries_api/src/component/Countries.js
+++ countries_api/src/component/Countries.js
@@ -1,11 +1,13 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Countries = () => {
+  const [countries, setCountries] = useState([]);
+
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
-      .then((res) => console.log(res.data));
+      .then((res) => setCountries(res.data));
   }, []);
   return <div></div>;
 };
