df65b9fdd9ba439cbf4857cd1a1904ee5b2b0ce5
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-15 22:23:32 +0100

    feat : update font responsive

--- countries_api/src/App.js
+++ countries_api/src/App.js
@@ -4,8 +4,10 @@ import Countries from "./component/Countries";
 const App = () => {
   return (
     <div className="flex flex-col items-center bg-gray-300 h-screen">
-      <h1 className="text-3xl font-extrabold p-20">Country Single Page</h1>
-      <ul className="text-white flex flex-row text-center justify-around w-[80%] bg-gray-800 rounded-full p-2">
+      <h1 className="text-3xl font-extrabold p-10 text-center w-[100%]">
+        Country Single Page
+      </h1>
+      <ul className="text-white text-xs sm:text-base flex flex-row text-center justify-around w-[95%] bg-gray-800 rounded-full p-2">
         <li className="hover:bg-gray-500 w-[100%] rounded-full">Europe</li>
         <li className="hover:bg-gray-500 w-[100%] rounded-full ">Afrique</li>
         <li className="hover:bg-gray-500 w-[100%] rounded-full ">Amérique</li>
