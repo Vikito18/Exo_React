ee46449177763694db6a17c68930a27109526f37
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-16 16:12:12 +0100

    feat : map countries name

--- countries_api/src/component/Countries.js
+++ countries_api/src/component/Countries.js
@@ -9,7 +9,16 @@ const Countries = () => {
       .get("https://restcountries.com/v3.1/all")
       .then((res) => setCountries(res.data));
   }, []);
-  return <div></div>;
+
+  return (
+    <ul className="flex flex-wrap">
+      {countries.map((country, index) => (
+        <li className="m-5 bg-gray-900 text-white rounded-xl" key={index}>
+          {country.translations.fra.common}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Countries;
