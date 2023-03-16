4f20054fd1e9cfeb9e1676b810663619c4d13ab0
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-16 17:01:49 +0100

    feat : map capital region & population

--- countries_api/src/component/Countries.js
+++ countries_api/src/component/Countries.js
@@ -11,16 +11,33 @@ const Countries = () => {
   }, []);
 
   return (
-    <ul className="flex flex-wrap justify-around">
+    <ul className="flex flex-wrap justify-around items-center">
       {countries.map((country, index) => (
         <li
-          className="m-5 bg-gray-900 text-white rounded-xl flex-flex-col"
+          className="m-5 pb-5 bg-gray-900 text-white h-min rounded-3xl flex-flex-col w-[80%] sm:w-[30%] md:w-[25%]"
           key={index}
         >
-          <img src={country.flags.png} />
-          <h2 className="font-extrabold text-center text-lg">
+          <img
+            className="rounded-t-3xl w-[100%]"
+            src={country.flags.png}
+            alt={country.translations.fra.common}
+          />
+          <h2 className="font-extrabold text-center text-2xl mb-5">
             {country.translations.fra.common}{" "}
           </h2>
+          <div className="flex flex-col items-center">
+            <p>
+              Capitale :{" "}
+              <span className="font-extrabold">{country.capital}</span>
+            </p>
+            <p>
+              Region : <span className="font-extrabold">{country.region}</span>
+            </p>
+            <p>
+              Population :{" "}
+              <span className="font-extrabold">{country.population}</span>
+            </p>
+          </div>
         </li>
       ))}
     </ul>
