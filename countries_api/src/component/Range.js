fe81cad95ae61ecf1fe6de209bc3915ba9565693
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-17 15:20:43 +0100

    feat : event input range

--- countries_api/src/component/Range.js
+++ countries_api/src/component/Range.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Range = () => {
+  const [rangeValue, setRangeValue] = useState(100);
+  const onChange = (e) => {
+    setRangeValue(e.target.value);
+  };
   return (
     <div className="flex justify-center pt-5">
-      <input className="w-[50%] flex" type="range" min="1" max="250"></input>
+      <input
+        className="w-[50%] flex"
+        type="range"
+        min="1"
+        max="250"
+        defaultValue={rangeValue}
+        onChange={onChange}
+      ></input>
     </div>
   );
 };
