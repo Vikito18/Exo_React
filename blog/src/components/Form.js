939466d9c18db1b45c4c501e1b04cb23b1dbc950
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-19 17:02:46 +0100

    handle error

--- blog/src/components/Form.js
+++ blog/src/components/Form.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 
 const Form = () => {
   const [textContent, setTextContent] = useState("");
+  const [error, setError] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    textContent.length < 140 ? setError(true) : setError(false);
   };
 
   const handleTextContent = (e) => {
@@ -25,6 +27,11 @@ const Form = () => {
         className="border-cyan-400 border rounded-lg p-1 w-[80%] h-40"
         placeholder="Message"
       />
+      {error && (
+        <p className="text-red-700 text-xs">
+          Veuillez écrire plus de 140 carcatères
+        </p>
+      )}
       <input
         className="bg-cyan-400 rounded-xl w-40 p-1 "
         type="submit"
