4a411472b52d89bb5f97cfa99c4a06b9498330c4
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-19 16:54:18 +0100

    event handleTextContent & handleSubmit

--- blog/src/components/Form.js
+++ blog/src/components/Form.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [textContent, setTextContent] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleTextContent = (e) => {
+    setTextContent(e.target.value);
+  };
   return (
-    <form className="flex flex-col items-center gap-5 w-[90%] ">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center gap-5 w-[90%] "
+    >
       <input
-        className="border-cyan-400 border-2 rounded-xl p-1 w-[40%] "
+        className="border-cyan-400 border rounded-lg p-1 w-[40%] "
         type="text"
         placeholder="nom"
       />
       <textarea
-        className="border-cyan-400 border-2 rounded-xl p-1 w-[80%] h-40"
+        onChange={handleTextContent}
+        className="border-cyan-400 border rounded-lg p-1 w-[80%] h-40"
         placeholder="Message"
       />
       <input
-        className="bg-cyan-400 rounded-xl w-40 p-1"
+        className="bg-cyan-400 rounded-xl w-40 p-1 "
         type="submit"
         value="Envoyer"
       />
