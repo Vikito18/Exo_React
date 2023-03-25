ec318626024e3cc4c8074bf0c32dffc771e64ec3
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-25 22:56:11 +0100

    feat : remove menu dropdown

--- test_portfolio/src/components/Page.js
+++ test_portfolio/src/components/Page.js
@@ -1,6 +1,6 @@
 import Nav from "./Nav";
 import { Menu } from "@headlessui/react";
-import { Fragment } from "react";
+import { NavLink } from "react-router-dom";
 
 export const PageHeader = (props) => {
   const { title } = props;
@@ -8,24 +8,10 @@ export const PageHeader = (props) => {
     <div className="flex-col flex items-center sm:items-center bg-gray-900">
       <h1 className="flex text-xxl text-cyan-500 font-extrabold"> {title}</h1>
       <ul className="flex-row pb-10 items-center items-around  sm:flex-row flex justify-around sm:w-[100%] mt-10  w-[80%]">
-        <Menu
-          as="div"
-          className="bg-gray-800 flex items-center justify-center p-10 rounded-xl border-cyan-500 border-solid border-2 h-[50%] w-[30%] hover:bg-cyan-500 hover:text-black"
-        >
-          <Menu.Button className="w-full h-20">Portfolio</Menu.Button>
-          <Menu.Items className="flex flex-col ">
-            <Menu.Item className="absolute right-0 mt-10 w-[100%] origin-top-left">
-              <div className="flex flex-col text-cyan-500">
-                <Nav path="/" title="Projet 1" />
-                <Nav path="/" title="Projet 2" />
-                <Nav path="/" title="Projet 3" />
-              </div>
-            </Menu.Item>
-          </Menu.Items>
-        </Menu>
+        <Nav path="/" title="Home" />
         <Nav path="/Cv" title="Mon CV" />
         <Nav path="/Contact" title="Contact" />
-        <Nav path="/" title="Home" />
+        <Nav path="/" title="Portfolio" />
       </ul>
     </div>
   );
