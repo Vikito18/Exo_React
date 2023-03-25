4af5b5d32762631d193a22f478635db169772e27
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-25 20:32:44 +0100

    feat : menu dropdown

--- test_portfolio/src/components/Page.js
+++ test_portfolio/src/components/Page.js
@@ -1,4 +1,6 @@
 import Nav from "./Nav";
+import { Menu } from "@headlessui/react";
+import { Fragment } from "react";
 
 export const PageHeader = (props) => {
   const { title } = props;
@@ -9,7 +11,25 @@ export const PageHeader = (props) => {
         <Nav path="/" title="Home" />
         <Nav path="/Cv" title="Mon CV" />
         <Nav path="/Contact" title="Contact" />
-        <Nav path="/" title="Portfolio"></Nav>
+        <Menu>
+          <Menu.Button className="bg-gray-800 flex items-center justify-center p-10 rounded-xl border-cyan-500 border-solid border-2 h-[50%] w-[30%] hover:bg-cyan-500 hover:text-black">
+            Portfolio
+          </Menu.Button>
+          <Menu.Items>
+            <Menu.Item>
+              {({ active }) => (
+                <>
+                  <Nav path="/" title="Projet 1" />
+                  <Nav path="/" title="Projet 2" />
+                  <Nav path="/" title="Projet 3" />
+                </>
+              )}
+            </Menu.Item>
+            <Menu.Item disabled>
+              <span className="opacity-75">Invite a friend (coming soon!)</span>
+            </Menu.Item>
+          </Menu.Items>
+        </Menu>
       </ul>
     </div>
   );
