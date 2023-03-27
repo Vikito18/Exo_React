2fb094f2ec07297ce0326a0d1eeb9dadbfab8455
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-27 16:49:41 +0200

    feat : update display

--- test_portfolio/src/pages/Cv.js
+++ test_portfolio/src/pages/Cv.js
@@ -7,63 +7,67 @@ import knex from "../img/knex.png";
 import bootstrap from "../img/bootstrap.png";
 import tailwind from "../img/tailwind.png";
 import psql from "../img/psql.png";
-import profile from "../img/profile.png";
+import MovingComponent from "react-moving-text";
+import SocialNetwork from "../components/SocialNetwork";
 
 const Cv = () => {
   return (
     <Page title="Mon CV">
-      <div className=" flex flex-col self-center justify-center w-[100%] text-lg sm:text-xl lg:text-xxl font-extrabold  ">
-        <div className="flex flex-row items-center pb-5 justify-around sm:justify-around w-[100%]">
-          <img
-            src={profile}
-            alt="photo_profile"
-            className="w-[15%] rounded-full"
-          />
-          <h1 className="flex self-center justify-self-center">
-            Victor DOS MARTIRES
-          </h1>
-        </div>
-        <div className="flex flex-col md:flex-row justify-around items-center  w-[100%]">
-          <section className="flex flex-col items-center rounded-full bg-gray-200 border-4 border-cyan-500 p-5 w-[90%] md:w-[50%]">
-            <h1>Skills</h1>
-            <img className="w-[40%] pt-10" src={logo} alt="logo_html_css_js" />
-            <div className="flex flex-row justify-around">
-              <img className="w-[20%] pt-10" src={react} alt="logo_react" />
-              <img className="w-[15%] pt-10" src={node} alt="logo_nodeJs" />
-              <img className="w-[15%] pt-10" src={knex} alt="logo_knexJs" />
-            </div>
-            <div className="flex flex-row justify-around">
-              <img
-                className="w-[20%] pt-10"
-                src={bootstrap}
-                alt="logo_bootstrap"
-              />
-              <img
-                className="w-[30%] pt-10"
-                src={tailwind}
-                alt="logo_tailwind"
-              />
-            </div>
-            <img className="w-[20%] pt-10" src={psql} alt="logo_psql" />
-          </section>
-          <section className="flex flex-col mt-20 sm:mt-0 justify-center items-center rounded-full border-4 border-cyan-500 bg-gray-200  items-center pt-5 h-full w-[80%] sm:w-[45%]">
-            <h1>Parcours</h1>
-            <ul className="lg:text-lg text-base flex flex-col text-center items-center sm:w-[75%]">
-              <li className="flex pt-10 pb-8 w-[100%]">
-                Certificat professionnel Développeur et Concepteur d'application
-                mobile
-              </li>
-              <li className=" pb-8">
-                Cértificat Développeur d'application web /web mobile
-              </li>
-              <li className=" pb-14">
-                Bachelor en Management Marketing événementiel
-              </li>
-              <li></li>
-            </ul>
-          </section>
-        </div>
+      <div className="flex flex-col gap-5 md:flex-row justify-around items-center w-[100%] font-extrabold">
+        <MovingComponent
+          className="flex flex-col items-center rounded-3xl bg-gray-200 border-4 border-cyan-500  w-auto "
+          type="fadeInFromLeft"
+          duration="1500ms"
+          delay="0s"
+          direction="normal"
+          timing="ease"
+          iteration="1"
+          fillMode="none"
+        >
+          <img className="w-64 h-32" src={logo} alt="logo_html_css_js" />
+          <div className="flex flex-row justify-center sm:gap-20 w-[100%]">
+            <img className="w-28 h-32 pt-10" src={react} alt="logo_react" />
+            <img className="w-28 h-32 pt-10" src={node} alt="logo_nodeJs" />
+            <img className="w-28 h-32 pt-10" src={knex} alt="logo_knexJs" />
+          </div>
+          <div className="flex flex-row justify-center pb-5 sm:gap-20 w-[100%]">
+            <img
+              className="w-28 h-32 pt-10"
+              src={bootstrap}
+              alt="logo_bootstrap"
+            />
+            <img
+              className="w-28 h-32 pt-10"
+              src={tailwind}
+              alt="logo_tailwind"
+            />
+            <img className="w-28 h-32 pt-10" src={psql} alt="logo_psql" />
+          </div>
+        </MovingComponent>
+
+        <MovingComponent
+          className="flex flex-col sm:mt-0 justify-center items-center rounded-2xl border-4 border-cyan-500 bg-gray-200  items-center h-full w-72 sm:w-96 "
+          type="fadeInFromLeft"
+          duration="800ms"
+          delay="s"
+          direction="normal"
+          timing="ease"
+          iteration="1"
+          fillMode="none"
+        >
+          <ul className=" text-lg flex flex-col text-center items-center w-[95%]">
+            <li className="flex pb-8 w-[90%]">
+              Certificat professionnel Développeur et Concepteur d'application
+              mobile
+            </li>
+            <li className="w-[90%]">
+              Cértificat Développeur d'application web /web mobile
+            </li>
+            <li>Bachelor en Management Marketing événementiel</li>
+          </ul>
+        </MovingComponent>
       </div>
+      <SocialNetwork />
     </Page>
   );
 };
--- test_portfolio/src/pages/Home.js
+++ test_portfolio/src/pages/Home.js
@@ -2,27 +2,38 @@ import React from "react";
 import Page from "../components/Page";
 import SocialNetwork from "../components/SocialNetwork";
 import profile from "../img/profile.png";
+import MovingComponent from "react-moving-text";
 
 const Home = () => {
   return (
     <Page title="Welcome">
-      <div className="flex flex-col sm:flex-row  justify-around items-center w-[100%]">
-        <img
-          src={profile}
-          alt="potho_profile"
-          className="w-[30%] rounded-full"
-        />
-        <h2 className="flex flex-col text-xl lg:text-xxl font-extrabold ">
-          <span className="flex self-center">Victor</span>{" "}
-          <span className="flex self-center">DOS MARTIRES</span>
-          <p className="lg:pt-10  pt-5 flex flex-col lg:flex-row self-center">
-            Développeur{" "}
-            <span className="flex lg:self-center justify-self-center self-center text-yellow-600">
-              JavaScript
-            </span>
-          </p>
-        </h2>
-      </div>
+      <MovingComponent
+        type="fadeInFromLeft"
+        duration="1200ms"
+        delay="0s"
+        direction="normal"
+        timing="ease"
+        iteration="1"
+        fillMode="none"
+      >
+        <div className="flex flex-col sm:flex-row  justify-around items-center w-[100%]">
+          <img
+            src={profile}
+            alt="potho_profile"
+            className="w-[30%] rounded-full"
+          />
+          <h2 className="flex flex-col text-xl lg:text-xxl font-extrabold ">
+            <span className="flex self-center">Victor</span>{" "}
+            <span className="flex self-center">DOS MARTIRES</span>
+            <p className="lg:pt-10  pt-5 flex flex-col lg:flex-row self-center">
+              Développeur{" "}
+              <span className="flex lg:self-center justify-self-center self-center text-yellow-600">
+                JavaScript
+              </span>
+            </p>
+          </h2>
+        </div>
+      </MovingComponent>
       <SocialNetwork />
     </Page>
   );
