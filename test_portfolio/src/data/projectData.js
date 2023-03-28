85e099f91865e287689407e5ddeba1e5b495e9eb
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-03-29 00:55:52 +0200

    doc : data added

---
+++ test_portfolio/src/data/projectData.js
@@ -0,0 +1,27 @@
+export const ProjectData = [
+  {
+    id: 1,
+    title: "Pokedex",
+    language: ["HTML, JavaScript, CSS"],
+  },
+  {
+    id: 2,
+    title: "User API",
+    language: ["HTML", "JavaScript", "CSS"],
+  },
+  {
+    id: 3,
+    title: "Flag App",
+    language: ["React", "Tailwindcss"],
+  },
+  {
+    id: 4,
+    title: "Cooking API",
+    language: ["React", "Tailwindcss"],
+  },
+  {
+    id: 5,
+    title: "Blog",
+    language: ["React", "Tailwindcss", "json-server"],
+  },
+];
