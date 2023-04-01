b719da175e8d4816a9fa81f686f7a2cfaafd50e1
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-04-01 12:39:40 +0200

    feat : Router init & component Page init

--- app_movies/src/App.js
+++ app_movies/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ app_movies/src/components/Page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="bg-gray-900 min-h-screen text-white">
+      <h1 className="flex justify-center font-mono text-3xl font-extrabold">
+        AppsMovies
+      </h1>
+    </div>
+  );
+};
+
+export default Page;
