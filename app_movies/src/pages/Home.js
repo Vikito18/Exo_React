e480b01f7d912fbe6f383d5e363696e2196323b7
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-04-02 11:48:33 +0200

    feat : fetch data

--- app_movies/src/pages/Home.js
+++ app_movies/src/pages/Home.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Page from "../components/Page";
+import axios from "axios";
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=5b8d228ebdcaadeb58487e4a56841eaf&query=ALL`
+      )
+      .then((res) => setMovies(res.data.results));
+  });
+
   return (
     <Page>
       <div className="flex flex-col items-center">
