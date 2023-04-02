75ec71c5d60aa796e2bde593ccc2baccf02639ca
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-04-02 11:10:20 +0200

    feat : input ome

--- app_movies/src/components/Page.js
+++ app_movies/src/components/Page.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Nav from "./Nav";
 
-const Page = () => {
+const Page = ({ children }) => {
   return (
     <div className="bg-gray-900 min-h-screen text-white">
       <ul className="flex flex-row gap-5 absolute left-5 top-5 text-xl">
         <Nav path="/" pathName="Home" />
         <Nav path="/Favorite" pathName="Favoris" />
       </ul>
-      <h1 className="flex justify-center font-mono text-3xl font-extrabold pt-5">
+      <h1 className="flex justify-center font-mono text-3xl font-extrabold pt-20 mb-10">
         AppsMovies
       </h1>
+      {children}
     </div>
   );
 };
--- app_movies/src/pages/Home.js
+++ app_movies/src/pages/Home.js
@@ -4,9 +4,13 @@ import Page from "../components/Page";
 const Home = () => {
   return (
     <Page>
-      <form>
-        <input type="text" />
-      </form>
+      <div className="flex flex-col items-center">
+        <input
+          className="rounded-lg p-2 w-52"
+          type="text"
+          placeholder="Nom d'un film"
+        />
+      </div>
     </Page>
   );
 };
