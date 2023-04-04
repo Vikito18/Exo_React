1dbc1062addb3a453c697a3607cd00f252e6a17f
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-04-04 23:45:07 +0200

    feat : button tri

--- app_movies/src/pages/Home.js
+++ app_movies/src/pages/Home.js
@@ -15,10 +15,22 @@ const Home = () => {
       <div className="flex flex-col items-center">
         <input
           onChange={handleInput}
-          className="rounded-lg p-2 w-52 text-black mb-20"
+          className="rounded-lg p-2 w-52 text-black mb-5"
           type="text"
           placeholder="Nom d'un film"
         />
+        <div className="flex flex-row mb-16">
+          <input
+            className="rounded-lg bg-cyan-400 p-2 w-52 text-black mb-20"
+            type="button"
+            value="Top"
+          />
+          <input
+            className="rounded-lg bg-cyan-400 p-2 w-52 text-black mb-20"
+            type="button"
+            value="Flop"
+          />
+        </div>
         <div className="flex flex-wrap justify-center gap-5">
           <FetchMovies value={inputValue} />
         </div>
