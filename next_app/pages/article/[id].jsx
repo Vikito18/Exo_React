9a319f2eb40e1eb68bfbd496bc1c7509b30743be
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-04-09 18:03:08 +0200

    feat : article[id] style

--- next_app/pages/article/[id].jsx
+++ next_app/pages/article/[id].jsx
@@ -3,10 +3,12 @@ import React from "react";
 
 const article = ({ article }) => {
   return (
-    <div>
-      <h2> {article.title}</h2>
+    <div className="flex flex-col m-5 w-[90%] p-2 border-2 border-pink-600 mx-auto">
+      <h2 className="text-xl font-extrabold"> {article.title}</h2>
       <p>{article.body}</p>
-      <Link href="/">Revenir à l'acceuil</Link>
+      <Link className="bg-cyan-500 mx-auto rounded-lg p-2 mt-10 " href="/">
+        Revenir à l'acceuil
+      </Link>
     </div>
   );
 };
