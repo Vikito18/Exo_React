2553b674abf27a3f1d1f3efe63abe1540685a5b5
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-04-09 18:06:31 +0200

    feat : all article[id] page linked

--- next_app/components/Article.jsx
+++ next_app/components/Article.jsx
@@ -5,7 +5,7 @@ const Article = ({ article }) => {
   return (
     <Link
       className="flex flex-col items-center p-2 w-72 h-44 mx-auto border-2 border-pink-6 00 rounded-xl shadow-gray-600 shadow-lg"
-      href=""
+      href={`/article/${article.id}`}
     >
       <h3 className="text-lg font-bold text-center pb-2">{article.title} ➡️</h3>
       <p className="text-clip overflow-scroll">{article.body}</p>
