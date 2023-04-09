3002135a0f32aaf809899c04e8c78d82c73f4738
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-04-09 15:38:31 +0200

    feat : component Header

---
+++ next_app/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col items-center p-2">
+      <p className="text-2xl">Welcome to the website of :</p>
+      <h1 className="text-5xl font-extrabold">Victor DOS MARTIRES</h1>
+    </div>
+  );
+};
+
+export default Header;
--- next_app/components/Layout.jsx
+++ next_app/components/Layout.jsx
@@ -1,9 +1,11 @@
+import Header from "@/components/Header";
 import Navigation from "@/components/Navigation";
 import React from "react";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Header />
       <Navigation />
       <main>{children}</main>
     </>
