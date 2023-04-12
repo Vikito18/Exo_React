4428ae732eba748d10296aa28d65f6dcf6a1eed1
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-04-12 06:26:48 +0200

    update store of provider

--- redux_blog/src/index.js
+++ redux_blog/src/index.js
@@ -13,8 +13,8 @@ const store = configureStore({
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode store={store}>
-    <Provider>
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
